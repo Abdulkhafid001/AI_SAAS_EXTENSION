06d8b61bd7b42f3cdc9e5e28e8c7fb6d9eb02ea7
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-10-23 16:57:55 +0100

    added webpack to bundle scripts

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./keys.js", // Entry point for your JS files
+  output: {
+    filename: "bundle.js", // Output file
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development", // Or 'production' for final builds
+};
