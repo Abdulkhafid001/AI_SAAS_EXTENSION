96fd37b49ae54a30cc2dea3167ca44f950fd0258
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-10-24 14:43:53 +0100

    adding time calculation

--- scripts/content.js
+++ scripts/content.js
@@ -24,7 +24,6 @@ document.addEventListener("mouseup", function () {
 
 const article = document.querySelector("article");
 if (article) {
-  
   const text = article.textContent;
   const wordMatchRegExp = /[^\s]+/g;
   const words = text.matchAll(wordMatchRegExp);
