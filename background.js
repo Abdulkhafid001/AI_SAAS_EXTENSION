3af8a29edde41e3c316c79e416156db968f08140
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-10-26 09:57:28 +0100

    add web bundler

--- background.js
+++ background.js
@@ -1,38 +1,42 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: "OFF",
-  });
-});
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "textSelected") {
-    console.log("Selected text:", message.text);
-  }
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.action.setBadgeText({
+//     text: "OFF",
+//   });
+// });
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.action === "textSelected") {
+//     console.log("Selected text:", message.text);
+//   }
+// });
 
-const extensions = "https://developer.chrome.com/docs/extensions";
-const djangodocs = "https://docs.djangoproject.com/en/5.1/";
+// const extensions = "https://developer.chrome.com/docs/extensions";
+// const djangodocs = "https://docs.djangoproject.com/en/5.1/";
 
-chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(extensions) || tab.url.startsWith(djangodocs)) {
-    const previousState = await chrome.action.getBadgeText({ tabId: tab.id });
+// chrome.action.onClicked.addListener(async (tab) => {
+//   if (tab.url.startsWith(extensions) || tab.url.startsWith(djangodocs)) {
+//     const previousState = await chrome.action.getBadgeText({ tabId: tab.id });
 
-    const nextState = previousState === "ON" ? "OFF" : "ON";
+//     const nextState = previousState === "ON" ? "OFF" : "ON";
 
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
+//     await chrome.action.setBadgeText({
+//       tabId: tab.id,
+//       text: nextState,
+//     });
 
-    if (nextState === "ON") {
-      chrome.scripting.insertCSS({
-        files: ["focus_mode.css"],
-        target: { tabId: tab.id },
-      });
-    } else if (nextState === "OFF") {
-      chrome.scripting.removeCSS({
-        files: ["focus_mode.css"],
-        target: { tabId: tab.id },
-      });
-    }
-  }
-});
+//     if (nextState === "ON") {
+//       chrome.scripting.insertCSS({
+//         files: ["focus_mode.css"],
+//         target: { tabId: tab.id },
+//       });
+//     } else if (nextState === "OFF") {
+//       chrome.scripting.removeCSS({
+//         files: ["focus_mode.css"],
+//         target: { tabId: tab.id },
+//       });
+//     }
+//   }
+// });
+
+
+import './sw-omnibox.js';
+import './sw-tips.js';
--- scripts/content.js
+++ scripts/content.js
@@ -1,44 +1,44 @@
-chrome.runtime.onMessage.addListener((request) => {
-  if (request.action === "toggleHighLightMode") {
-    chrome.storage.local.set({ isHighLightMode: request.enable }, function () {
-      console.log(`Highlight mode is now: ${request.enable}`);
-    });
-  }
-});
+// chrome.runtime.onMessage.addListener((request) => {
+//   if (request.action === "toggleHighLightMode") {
+//     chrome.storage.local.set({ isHighLightMode: request.enable }, function () {
+//       console.log(`Highlight mode is now: ${request.enable}`);
+//     });
+//   }
+// });
 
-// When text is selected, check the state from storage
-document.addEventListener("mouseup", function () {
-  chrome.storage.local.get(["isHighLightMode"], function (result) {
-    if (result.isHighLightMode) {
-      const selectedText = window.getSelection().toString().trim();
-      chrome.runtime.sendMessage({
-        action: "textSelected",
-        text: selectedText,
-      });
-      if (selectedText) {
-        alert(`Selected text: ${selectedText}`);
-      }
-    }
-  });
-});
+// // When text is selected, check the state from storage
+// document.addEventListener("mouseup", function () {
+//   chrome.storage.local.get(["isHighLightMode"], function (result) {
+//     if (result.isHighLightMode) {
+//       const selectedText = window.getSelection().toString().trim();
+//       chrome.runtime.sendMessage({
+//         action: "textSelected",
+//         text: selectedText,
+//       });
+//       if (selectedText) {
+//         alert(`Selected text: ${selectedText}`);
+//       }
+//     }
+//   });
+// });
 
-const article = document.querySelector("body");
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g;
-  const words = text.matchAll(wordMatchRegExp);
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  console.log("word in page: " + readingTime);
+// const article = document.querySelector("body");
+// if (article) {
+//   const text = article.textContent;
+//   const wordMatchRegExp = /[^\s]+/g;
+//   const words = text.matchAll(wordMatchRegExp);
+//   const wordCount = [...words].length;
+//   const readingTime = Math.round(wordCount / 200);
+//   console.log("word in page: " + readingTime);
 
-  const badge = document.createElement("p");
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
+//   const badge = document.createElement("p");
+//   badge.classList.add("color-secondary-text", "type--caption");
+//   badge.textContent = `⏱️ ${readingTime} min read`;
 
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
+//   // Support for API reference docs
+//   const heading = article.querySelector("h1");
+//   // Support for article docs with date
+//   const date = article.querySelector("time")?.parentNode;
 
-  (date ?? heading).insertAdjacentElement("afterend", badge);
-}
+//   (date ?? heading).insertAdjacentElement("afterend", badge);
+// }
