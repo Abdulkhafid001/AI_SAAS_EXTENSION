b88626507b6527b1649780f5f0ad71cd026eb459
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-10-26 10:13:06 +0100

    added chrome docs

--- background.js
+++ background.js
@@ -37,6 +37,13 @@
 //   }
 // });
 
+import "./sw-omnibox.js";
+import "./sw-tips.js";
 
-import './sw-omnibox.js';
-import './sw-tips.js';
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+  if (reason == "install") {
+    chrome.storage.local.set({
+      apiSuggestions: ["tabs", "storage", "scripting"],
+    });
+  }
+});
