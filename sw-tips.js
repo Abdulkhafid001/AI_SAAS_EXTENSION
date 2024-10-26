3e0519208ef886f0d74fe0403b28f33fb6cffda4
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-10-26 11:47:19 +0100

    implementing popups

--- sw-tips.js
+++ sw-tips.js
@@ -1,1 +1,23 @@
-console.log("sw-tips.js");
+const fetchAndSaveTipInStorage = async () => {
+  const response = await fetch("https://extension-tips.glitch.me/tips.json");
+  const tips = await response.json();
+  const randomIndex = Math.floor(Math.random() * tips.length);
+  return chrome.storage.local.set({ tip: tips[randomIndex] });
+};
+
+const ALARM_NAME = "tip";
+
+async function createAlarm() {
+  const alarm = await chrome.alarms.get(ALARM_NAME);
+  if (typeof alarm === "undefined") {
+    chrome.alarms.create(ALARM_NAME, {
+      delayInMinutes: 1,
+      periodInMinutes: 1440,
+    });
+    fetchAndSaveTipInStorage();
+  }
+}
+
+createAlarm();
+
+chrome.alarms.onAlarm.addListener(fetchAndSaveTipInStorage);
