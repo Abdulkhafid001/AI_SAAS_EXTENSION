08085223d5c33ebd3051981b3b05df1bb3b5ff22
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-10-26 12:12:17 +0100

    completed tooltip

--- sw-tips.js
+++ sw-tips.js
@@ -21,3 +21,10 @@ async function createAlarm() {
 createAlarm();
 
 chrome.alarms.onAlarm.addListener(fetchAndSaveTipInStorage);
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.greeting === "tip") {
+    chrome.storage.local.get("tip").then(sendResponse);
+    return true;
+  }
+});
