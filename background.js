a86eda02c6c1f3ebb5028738ce4d93a099bb654e
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-11-02 01:16:47 +0100

    changing the extension

--- background.js
+++ background.js
@@ -36,14 +36,3 @@
 //     }
 //   }
 // });
-
-import "./sw-omnibox.js";
-import "./sw-tips.js";
-
-chrome.runtime.onInstalled.addListener(({ reason }) => {
-  if (reason == "install") {
-    chrome.storage.local.set({
-      apiSuggestions: ["tabs", "storage", "scripting"],
-    });
-  }
-});
