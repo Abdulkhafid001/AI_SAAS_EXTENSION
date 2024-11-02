883ec8508e6d6b4e8d5745eb7f1bfbbacb9ec4ba
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-11-02 01:23:35 +0100

    adding popup

--- background.js
+++ background.js
@@ -3,36 +3,8 @@
 //     text: "OFF",
 //   });
 // });
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//   if (message.action === "textSelected") {
-//     console.log("Selected text:", message.text);
-//   }
-// });
-
-// const extensions = "https://developer.chrome.com/docs/extensions";
-// const djangodocs = "https://docs.djangoproject.com/en/5.1/";
-
-// chrome.action.onClicked.addListener(async (tab) => {
-//   if (tab.url.startsWith(extensions) || tab.url.startsWith(djangodocs)) {
-//     const previousState = await chrome.action.getBadgeText({ tabId: tab.id });
-
-//     const nextState = previousState === "ON" ? "OFF" : "ON";
-
-//     await chrome.action.setBadgeText({
-//       tabId: tab.id,
-//       text: nextState,
-//     });
-
-//     if (nextState === "ON") {
-//       chrome.scripting.insertCSS({
-//         files: ["focus_mode.css"],
-//         target: { tabId: tab.id },
-//       });
-//     } else if (nextState === "OFF") {
-//       chrome.scripting.removeCSS({
-//         files: ["focus_mode.css"],
-//         target: { tabId: tab.id },
-//       });
-//     }
-//   }
-// });
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "textSelected") {
+    console.log("Selected text:", message.text);
+  }
+});
